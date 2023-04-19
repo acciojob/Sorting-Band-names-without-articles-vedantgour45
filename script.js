6396b16db436ecc9762f3f1d0f8c8df0178c361a
Vedant Gour <113295244+vedantgour45@users.noreply.github.com>
2023-04-19 09:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
 
+let touristSpots=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function strip(bandname){
+	return bandname.replace(/^(a|the|an)/i,'').trim();
+	
+}
+const sortedBands=touristSpots.sort((a,b)=>strip(a)>strip(b)?1:-1);
+document.querySelector('#bands').innerHTML=sortedBands.map(band=>
+	`<li>${band}</li>`).join(``)
+console.log(sortedBands);
